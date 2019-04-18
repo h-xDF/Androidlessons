fae8bddec7a65b4b2c220eb534f6d1ce62984d7a
h-xDF <raralux@yandex.ru>
2019-04-19 02:00:48 +0300

    24 lesson(life cicle for 3 activity)

---
+++ p0241_twoactivitystate/src/main/java/ru/startandroid/p0241_twoactivitystate/ThreeActivity.java
@@ -0,0 +1,53 @@
+package ru.startandroid.p0241_twoactivitystate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ThreeActivity extends AppCompatActivity {
+
+    final String TAG = "States";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three);
+        Log.d(TAG, "ActivityThree: onCreate()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "ActivityThree: onRestart()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "ActivityThree: onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "ActivityThree: onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "ActivityThree: onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "ActivityThree: onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "ActivityThree: onDestroy()");
+    }
+}
