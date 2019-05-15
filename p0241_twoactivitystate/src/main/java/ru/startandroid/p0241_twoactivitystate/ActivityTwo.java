48b5a1e5792a7ca087399fda5ac3b2c9837f3363
h-xDF <raralux@yandex.ru>
2019-05-15 15:31:44 +0300

    wip: test

--- p0241_twoactivitystate/src/main/java/ru/startandroid/p0241_twoactivitystate/ActivityTwo.java
+++ p0241_twoactivitystate/src/main/java/ru/startandroid/p0241_twoactivitystate/ActivityTwo.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class ActivityTwo extends AppCompatActivity implements View.OnClickListener {
-
     final String TAG = "States";
     Button btnActThree;
 
