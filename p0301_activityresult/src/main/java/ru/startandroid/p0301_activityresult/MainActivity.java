31a1df70d8aff47bb46eef66cf32d7984624c323
h-xDF <raralux@yandex.ru>
2019-05-26 19:51:21 +0300

    30 lesson
    ActivityForResult - request code

---
+++ p0301_activityresult/src/main/java/ru/startandroid/p0301_activityresult/MainActivity.java
@@ -0,0 +1,72 @@
+package ru.startandroid.p0301_activityresult;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    final int REQUEST_CODE_COLOR = 1;
+    final int REQUEST_CODE_ALIGN = 2;
+
+    TextView tvText;
+    Button btnColor;
+    Button btnAlign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvText = findViewById(R.id.tvText);
+        btnColor = findViewById(R.id.btnColor);
+        btnAlign = findViewById(R.id.btnAlign);
+
+        btnColor.setOnClickListener(this);
+        btnAlign.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btnColor:
+                intent = new Intent(this, ColorActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_COLOR);
+                break;
+            case R.id.btnAlign:
+                intent = new Intent(this, AlignActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_ALIGN);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("myLogs", "requestCode = " + requestCode + " , resultCode = " + resultCode);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_COLOR:
+                    int color = data.getIntExtra("color", Color.WHITE);
+                    tvText.setTextColor(color);
+                    break;
+                case REQUEST_CODE_ALIGN:
+                    int alignment = data.getIntExtra("alignment", Gravity.LEFT);
+                    tvText.setGravity(alignment);
+                    break;
+            }
+        } else {
+            Toast.makeText(this, "Wrong result", Toast.LENGTH_LONG).show();
+        }
+    }
+}
