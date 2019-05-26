2a572ccde5d3a13049f135151eacdd0384c9f2df
h-xDF <raralux@yandex.ru>
2019-05-26 16:23:10 +0300

    29 lesson
    ActivityForResult

---
+++ p0291_sampleactivityresult/src/main/java/ru/startandroid/p0291_sampleactivityresult/NameActivity.java
@@ -0,0 +1,31 @@
+package ru.startandroid.p0291_sampleactivityresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NameActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etName;
+    Button btnOK;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.name);
+
+        etName = findViewById(R.id.etName);
+        btnOK = findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.putExtra("name", etName.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
